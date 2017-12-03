c8939203578fb08b6dcb33857a41e38a1f0629a0
m3ller <melcanwong@hotmail.com>
2017-12-02 22:18:36 -0800

    Add environment info.  Change code indentation

--- mountaincar.py
+++ mountaincar.py
@@ -1,12 +1,21 @@
 import gym
+import tensorflow as tf
 
-env = gym.make("MountainCar-v0")
-env.reset()
+# Environment documentation: https://github.com/openai/gym/wiki/MountainCar-v0
+# Actions: [0]left push, [1]no push, [2]right pushA
+# Observations: [0]position, [1]velocity
 
-for _ in xrange (100):
-    env.render()
-    action = env.action_space.sample()
-    observation, reward, done, info = env.step(action)
+def main():
+    env = gym.make("MountainCar-v0")
+    env.reset()
+    
+    for _ in xrange (100):
+        env.render()
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+    
+        if done:
+            break
 
-    if done:
-        break
+if __name__ == "__main__":
+    main()

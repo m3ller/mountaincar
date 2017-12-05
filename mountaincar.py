f7879e453cb00ae0a0e6d356d9765b2f28667242
m3ller <melcanwong@hotmail.com>
2017-12-04 19:02:11 -0800

    Changed indentation so that code is in a class.  Added the outlines of policy and value networks

--- mountaincar.py
+++ mountaincar.py
@@ -1,9 +1,25 @@
 import gym
 import tensorflow as tf
 
-# Environment documentation: https://github.com/openai/gym/wiki/MountainCar-v0
-# Actions: [0]left push, [1]no push, [2]right pushA
-# Observations: [0]position, [1]velocity
+class MountainCarLearner():
+  # Environment doc: https://github.com/openai/gym/wiki/MountainCar-v0
+  # Actions: [0]left push, [1]no push, [2]right pushA
+  # Observations: [0]position, [1]velocity
+  def __init__(self, environment):
+    ACT_SPACE = 3
+    OBS_SPACE = 2
+    env = environment
+ 
+  """ Policy network tries to learn the 'best' policy by trying to optimize
+  over the predicted reward (as given by value_grad(..)).
+  """
+  def policy_grad(self):
+      pass
+
+  """ Value network learns to predict the reward for a given action.
+  """
+  def value_grad(self):
+      pass
 
 def main():
     env = gym.make("MountainCar-v0")

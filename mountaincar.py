4bacb5b22b26d4815ba2fdd31a7fe9432c14da54
m3ller <melcanwong@hotmail.com>
2018-02-19 14:34:25 -0800

    Added a basic linear network to policy gradient.

--- mountaincar.py
+++ mountaincar.py
@@ -1,4 +1,5 @@
 import gym
+import numpy as np
 import tensorflow as tf
 
 class MountainCarLearner():
@@ -6,17 +7,26 @@ class MountainCarLearner():
   # Actions: [0]left push, [1]no push, [2]right push
   # Observations: [0]position, [1]velocity
   def __init__(self, environment):
-    ACT_SPACE = 3
-    OBS_SPACE = 2
-    env = environment
+    self.ACT_SPACE = 3
+    self.OBS_SPACE = 2
+    self.env = environment
 
   #TODO: add tensorboard
+  #TODO: batch the games
   """ Policy network tries to learn the 'best' policy by trying to optimize
   over the predicted reward (as given by value_grad(..)).
   """
   def policy_grad(self):
+      # Build network
+      observation = tf.placeholder(tf.float32, [1, self.OBS_SPACE])
+      w = tf.get_variable("pg_weight", [self.OBS_SPACE, self.ACT_SPACE])
+      b = tf.get_variable("pg_bias", [1, self.ACT_SPACE])
+
+      logp = tf.matmul(observation, w) + b
+      prob = tf.nn.softmax(logp)
+      # Produces an action probability and selects with this distribution
       # return action, act_loss, optimizer
-      pass
+      return observation, prob
 
   """ Value network learns to predict the reward (i.e. "future worth") of
   a given action.
@@ -38,14 +48,25 @@ class MountainCarLearner():
 def main():
     env = gym.make("MountainCar-v0")
     env.reset()
+
+    learner = MountainCarLearner(env)
+
+    # Build network
+    pg_obs, pg_prob = learner.policy_grad()
+   
+    # Run episode
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        action = env.action_space.sample()  # initial action
+
+        for _ in xrange (100):
+            env.render()
+            observation, reward, done, info = env.step(action)
+            action_prob = sess.run(pg_prob, feed_dict={pg_obs: np.expand_dims(observation, 0)})
+            action = np.argmax(action_prob)
     
-    for _ in xrange (100):
-        env.render()
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-    
-        if done:
-            break
+            if done:
+                break
 
 if __name__ == "__main__":
     main()

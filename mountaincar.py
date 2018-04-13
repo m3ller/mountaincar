e7557987dcb9f589be8ac9d30df6784bdfb9126b
m3ller <melcanwong@hotmail.com>
2018-04-13 00:05:10 -0700

    Using AdaGrad with a decaying learning rate

--- mountaincar.py
+++ mountaincar.py
@@ -45,7 +45,12 @@ class ReinforcementLearner():
         #adjustment = tf.reduce_sum(tf.multiply(prob, action), axis=1)
         adjustment = tf.multiply(adjustment, advantage)  # BE WARY OF BROADCASTING
         loss = -tf.reduce_sum(adjustment, axis=0) #+ reg_constant * tf.reduce_sum(reg_losses)
-        optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+        # optimizer
+        global_step = tf.Variable(0, trainable=False)
+        starter_learning_rate = 0.1
+        learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,100000, 0.96, staircase=True)
+        optimizer = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
 
         # Store on TensorBoard
         smy_pg_loss = tf.summary.scalar("pg_loss", loss)

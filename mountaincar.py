0121d35d472c042c13d3f41f093665ddf1835199
m3ller <melcanwong@hotmail.com>
2018-04-22 23:45:40 -0700

    Simplifying graphs and rewards

--- mountaincar.py
+++ mountaincar.py
@@ -12,114 +12,90 @@ class ReinforcementLearner():
         self.n_act = environment.action_space.n
         self.n_obs = environment.observation_space.shape[0]
   
-    #TODO: batch the games
-    """ Policy network tries to learn the 'best' policy by trying to optimize
-    over the predicted reward (as given by value_grad(..)).
-    """
     def policy_grad(self):
         # Build network
         global_step = tf.Variable(0, trainable=False)
         observation = tf.placeholder(tf.float32, [None, self.n_obs], "pg_obs")
 
-        n_hidden = 4
+        n_hidden = 16
         w1 = tf.get_variable("pg_w1", [self.n_obs, n_hidden])
         b1 = tf.get_variable("pg_b1", [1, n_hidden])
-        w2 = tf.get_variable("pg_w2", [n_hidden, n_hidden])
-        b2 = tf.get_variable("pg_b2", [1, n_hidden])
+        #w2 = tf.get_variable("pg_w2", [n_hidden, n_hidden])
+        #b2 = tf.get_variable("pg_b2", [1, n_hidden])
         w3 = tf.get_variable("pg_w3", [n_hidden, self.n_act])
         b3 = tf.get_variable("pg_b3", [1, self.n_act])
-        #w = tf.get_variable("pg_w", [self.n_obs, self.n_act])
 
         # Calculate probability
         temp_logp = tf.matmul(observation, w1) + b1
-        temp_logp = tf.nn.relu(temp_logp)
-        temp_logp = tf.matmul(temp_logp, w2) + b2
-        temp_logp = tf.nn.relu(temp_logp)
+        temp_logp = tf.nn.tanh(temp_logp)
+        #temp_logp = tf.matmul(temp_logp, w2) + b2
+        #temp_logp = tf.nn.relu(temp_logp)
         logp = tf.matmul(temp_logp, w3) + b3
-        #logp = tf.matmul(observation, w)
-        #prob = tf.nn.softmax(logp)
 
         # Epsilon Greedy
         threshold = tf.train.exponential_decay(1.0, global_step, 100, 0.8, staircase=True)
         prob = tf.cond(tf.less(tf.random_uniform([1])[0], threshold), lambda: tf.ones(tf.shape(logp), tf.float32), lambda: logp)
         prob = tf.nn.softmax(prob)
 
-        # Update network parameters with advantage
+        # Update network parameters with advantage and optimizer
         action = tf.placeholder(tf.float32, [None, self.n_act], "pg_act")
         advantage = tf.placeholder(tf.float32, [None], "pg_advantage")
 
         adjustment = tf.log(tf.reduce_sum(tf.multiply(prob, action), axis=1))
-        #adjustment = tf.reduce_sum(tf.multiply(prob, action), axis=1)
         adjustment = tf.multiply(adjustment, advantage)  # BE WARY OF BROADCASTING
-        loss = -tf.reduce_sum(adjustment, axis=0) #+ reg_constant * tf.reduce_sum(reg_losses)
+        loss = -tf.reduce_sum(adjustment, axis=0)
 
         # optimizer
-        starter_learning_rate = 0.1
-        learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,10000, 0.96, staircase=True)
-        optimizer = tf.train.AdagradOptimizer(learning_rate).minimize(loss, global_step=global_step)
+        optimizer = tf.train.AdamOptimizer().minimize(loss, global_step=global_step)
 
         # Store on TensorBoard
         smy_pg_loss = tf.summary.scalar("pg_loss", loss)
         smy_pg_prob = tf.summary.histogram("pg_prob", prob)
         smy_pg_thres = tf.summary.scalar("pg_theshold", threshold)
-        smy_pg_step = tf.summary.scalar("pg_step", global_step)
-        smy_op = tf.summary.merge([smy_pg_loss, smy_pg_prob, smy_pg_thres, smy_pg_step])
+        smy_op = tf.summary.merge([smy_pg_loss, smy_pg_prob, smy_pg_thres])
 
         return observation, prob, action, advantage, optimizer, smy_op
   
-    """ Value network learns to predict the EXPECTED reward of a given state.
-    """
     def value_grad(self):
         # Build network
         observation = tf.placeholder(tf.float32, [None, self.n_obs], "vg_obs")
         observed_value = tf.placeholder(tf.float32, [None, 1], "vg_value")
 
-        n_hidden = 5
+        n_hidden = 16
         w1 = tf.get_variable("vg_w1", [self.n_obs, n_hidden])
         b1 = tf.get_variable("vg_b1", [n_hidden])
-        w2 = tf.get_variable("vg_w2", [n_hidden, n_hidden])
-        b2 = tf.get_variable("vg_b2", [n_hidden])
+        #w2 = tf.get_variable("vg_w2", [n_hidden, n_hidden])
+        #b2 = tf.get_variable("vg_b2", [n_hidden])
         w3 = tf.get_variable("vg_w3", [n_hidden, 1])
         b3 = tf.get_variable("vg_b3", [1])
 
         temp_value = tf.matmul(observation, w1) + b1
-        temp_value = tf.nn.relu(temp_value)
-        temp_value = tf.matmul(temp_value, w2) + b2
-        temp_value = tf.nn.relu(temp_value)
+        temp_value = tf.nn.tanh(temp_value)
+        #temp_value = tf.matmul(temp_value, w2) + b2
+        #temp_value = tf.nn.relu(temp_value)
         expected_value = tf.matmul(temp_value, w3) + b3
 
         # Calculate and improve V(s) approximation
         diffs = observed_value - expected_value
-        #reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-        #reg_constant = 0.1
         loss = tf.nn.l2_loss(diffs)
 
         # optimizer
-        global_step = tf.Variable(0, trainable=False)
-        starter_learning_rate = 0.1
-        learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,10000, 0.96, staircase=True)
-        optimizer = tf.train.AdagradOptimizer(learning_rate).minimize(loss, global_step=global_step)
-        #optimizer = tf.train.AdamOptimizer().minimize(loss)
+        optimizer = tf.train.AdamOptimizer().minimize(loss)
 
         # Calculate the advantage
-        # TODO: May need to fiddle with this advantage
-        #advantages = tf.exp(diffs)
         advantages = diffs
 
         # Store on TensorBoard
         smy_vg_loss = tf.summary.scalar("vg_loss", loss)
         smy_vg_advantage = tf.summary.histogram("vg_advantage", advantages)
         summary_op = tf.summary.merge([smy_vg_loss, smy_vg_advantage])
-        return observation, observed_value, optimizer, advantages, summary_op
+        return observation, observed_value, optimizer, expected_value, summary_op
   
-    """ Go through one episode (i.e. game) of Mountain Car
-    """
     def run_episode(self, sess, pg_obs, pg_prob, render_flag=False):
         observations = []
         actions = []
         rewards = []
 
-        # Play through a game.  
         # Running policy_grad(observation) to produce actions in the game.
         observation = self.env.reset()   # initial observation
         for _ in xrange(200):
@@ -133,8 +109,7 @@ class ReinforcementLearner():
             # Store transistions and update
             observations.append(observation)
             actions.append(action)
-            add_reward = observation[0]**2 * 50
-            #add_reward = observation[0] * 5
+            add_reward = observation[0]*10
             rewards.append(reward + add_reward)
             observation = new_observation[:]
 
@@ -153,7 +128,6 @@ class ReinforcementLearner():
         return observations, action_onehot, rewards
 
     """ Update the parameters in the policy and value networks.
-
     Update is dependent on the observed rewards from the previous game.
     """
     def update_param(self, sess, transition_tuple, pg_obs, pg_prob, pg_action, pg_advantage, pg_optimizer, pg_summary_loss, vg_obs, vg_val, vg_optimizer, vg_advantage, vg_summary_loss):
@@ -165,8 +139,10 @@ class ReinforcementLearner():
             rewards[-i] += gamma * rewards[-i+1]    # Account for future reward
 
         # Calculate advantage
-        advantages = sess.run(vg_advantage,\
-                      feed_dict={vg_obs: observations, vg_val: rewards})
+        expected_value = sess.run(vg_advantage, feed_dict={vg_obs: observations})
+        advantages = rewards - expected_value
+        val_mean = np.mean(advantages)
+        advantages -= val_mean
 
         # Update value_grad
         _, vg_loss = sess.run([vg_optimizer, vg_summary_loss],\
@@ -181,7 +157,6 @@ class ReinforcementLearner():
   
 def main():
     env = gym.make("MountainCar-v0")
-    #env = gym.make("CartPole-v0")
 
     # Build network
     learner = ReinforcementLearner(env)
@@ -197,8 +172,7 @@ def main():
             transition_tuple = learner.run_episode(sess, pg_obs, pg_prob)
             vg_summary, pg_summary = learner.update_param(sess, transition_tuple, pg_obs, pg_prob, pg_act, pg_adv, pg_opt, pg_sumop, vg_obs, vg_val, vg_opt, vg_adv, vg_sumop)
 
-            #if i % 100 == 0:
-            if i % 2 == 0:
+            if i % 50 == 0:
                 train_writer.add_summary(vg_summary, i)
                 train_writer.add_summary(pg_summary, i)
          
